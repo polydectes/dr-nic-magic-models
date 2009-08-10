0f104f8f43ecd674dfc6e8414353b60ee38ad853
Alexandre Bini <alexandre@voraz.com.br>
2009-08-10 16:58:50 -0300

    ajustint validations

--- lib/dr_nic_magic_models/validations.rb
+++ lib/dr_nic_magic_models/validations.rb
@@ -14,39 +14,37 @@ module DrNicMagicModels
       # NOT NULL constraints
       self.columns.reject { |column| column.name =~ /(?i)^(((created|updated)_(at|on))|position|type|id)$/ }.each do |column|
         
-        inheritable = false
-        self.inheritable_attributes[:reflections].each_value{ |t| 
+        self.reflect_on_all_associations(:belongs_to).to_a.each{ |t| 
           if "#{t.name}_id" == column.name 
-            inheritable = true
             logger.debug "validates_associated #{t.name}"
             self.validates_associated t.name, :allow_nil=>column.null, :allow_blank=>column.null
             break
           end
-        }rescue 0
+        }
         
-        if !inheritable
-          if column.type == :integer
-            logger.debug "validates_numericality_of #{column.name}, :allow_nil => #{column.null.inspect}, :only_integer => true"       
-            self.validates_numericality_of column.name, :allow_nil => column.null, :only_integer => true
-          elsif column.number?
-            logger.debug "validates_numericality_of #{column.name}, :allow_nil => #{column.null.inspect}"
-            self.validates_numericality_of column.name, :allow_nil => column.null
-          elsif column.text? && column.limit
-            logger.debug "validates_length_of #{column.name}, :allow_nil => #{column.null.inspect}, :maximum => #{column.limit}"
-            self.validates_length_of column.name, :allow_nil => column.null, :maximum => column.limit
-          end
-
-          # Active record seems to interpolate booleans anyway to either true, false or nil...
-          if column.type == :boolean
-            logger.debug "validates_inclusion_of #{column.name}, :in => [true, false], :allow_nil => #{column.null}, :message => ActiveRecord::Errors.default_error_messages[:blank]"
-            self.validates_inclusion_of column.name, :in => [true, false], :allow_nil => column.null, :message => ActiveRecord::Errors.default_error_messages[:blank]
-          elsif !column.null
-            #test if the column have a belongs_to association
-            logger.debug "validates_presence_of #{column.name}"
-            self.validates_presence_of column.name
-          end
+        if column.type == :integer
+          logger.debug "validates_numericality_of #{column.name}, :allow_nil => #{column.null.inspect}, :only_integer => true"       
+          self.validates_numericality_of column.name, :allow_nil => column.null, :only_integer => true
+        elsif column.number?
+          logger.debug "validates_numericality_of #{column.name}, :allow_nil => #{column.null.inspect}"
+          self.validates_numericality_of column.name, :allow_nil => column.null
+        elsif column.text? && column.limit
+          logger.debug "validates_length_of #{column.name}, :allow_nil => #{column.null.inspect}, :maximum => #{column.limit}"
+          self.validates_length_of column.name, :allow_nil => column.null, :maximum => column.limit
         end
+     
+        # Active record seems to interpolate booleans anyway to either true, false or nil...
+        if column.type == :boolean
+          logger.debug "validates_inclusion_of #{column.name}, :in => [true, false], :allow_nil => #{column.null}, :message => ActiveRecord::Errors.default_error_messages[:blank]"
+          self.validates_inclusion_of column.name, :in => [true, false], :allow_nil => column.null, :message => ActiveRecord::Errors.default_error_messages[:blank]
+        elsif !column.null
+          #test if the column have a belongs_to association
+          logger.debug "validates_presence_of #{column.name}"
+          self.validates_presence_of column.name
+        end
+      
       end
+
 
       # Single-column UNIQUE indexes
       get_unique_index_columns.each do |col|
